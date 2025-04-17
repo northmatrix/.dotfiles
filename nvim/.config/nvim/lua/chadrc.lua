379d8f6040f3c9c2587acc8e3a4ae5b47494390e
Northmatrix <contact@northmatrix.co.uk>
2025-04-17 01:54:59 +0100

    new nvim update

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -6,19 +6,22 @@
 local M = {}
 
 M.base46 = {
-    theme = "ayu_dark",
-    -- hl_override = {
-    -- 	Comment = { italic = true },
-    -- 	["@comment"] = { italic = true },
-    -- },
+    theme = "onedark",
+
+    hl_override = {
+        Comment = { italic = true },
+        ["@comment"] = { italic = true },
+    },
 }
 
 M.nvdash = { load_on_startup = true }
-
--- M.ui = {
---     tabufline = {
---         lazyload = false
---   },
--- }
+M.ui = {
+    tabufline = {
+        lazyload = true,
+    },
+    statusline = {
+        separator_style = "block",
+    },
+}
 
 return M
--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -1,18 +1,18 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    asm = { "asmfmt"},
-    c =   { "clang-format" },
-    css = { "prettier" },
-    html = { "prettier" },
-    python = { "black" }
-  },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        css = { "prettier" },
+        html = { "prettier" },
+        rust = { "rustfmt" },
+        c = { "clang-format" },
+        python = { "black" },
+    },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
 }
 
 return options
