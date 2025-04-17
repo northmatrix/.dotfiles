bb883deff922140e970849f31ce4deb4fb06c15e
Northmatrix <contact@northmatrix.co.uk>
2025-04-18 00:42:27 +0100

    neovim rust

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,5 +1,5 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "pyright", "clangd", "rust_analyzer", "gopls" }
+local servers = { "html", "cssls", "pyright", "clangd", "gopls", "rust_analyzer" }
 
 vim.lsp.enable(servers)
