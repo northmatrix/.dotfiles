8f7c7fb7e2c920214f3b558920560328884ca8fd
Northmatrix <contact@northmatrix.co.uk>
2025-05-18 15:58:39 +0100

    sway is the way

--- sway/.config/sway/status.sh
+++ sway/.config/sway/status.sh
@@ -8,10 +8,11 @@ ICON_UPDATES=""      # nf-fa-download
 ICON_VPN=""
 ICON_CLIP=""         # nf-fa-file_text
 ICON_CPU=""         # fa-microchip
-ICON_MEM=""         # fa-line-chart
+ICON_MEM=""         # fa-line-chart
 ICON_VOLUME=""       # nf-fa-volume-up
 ICON_BRIGHTNESS=""   # nf-fa-sun-o
 ICON_MUSIC=""
+
 # Date and Time
 date_line="$ICON_DATE $(date '+%Y-%m-%d')"
 time_line="$ICON_CLOCK $(date '+%H:%M:%S')"
@@ -32,12 +33,12 @@ updates_line="$ICON_UPDATES $(pacman -Qu | wc -l)"
 wl_clipboard="$ICON_CLIP $(wl-paste | tr -cd '\11\12\15\40-\176' | head -c 8)..."
 
 # Memory usage
-memory_usage="$ICON_CPU MEM $(free | awk '/Mem:/ {printf "%.1f%%", $3/$2 * 100.0}')"
+memory_usage="$ICON_MEM $(free | awk '/Mem:/ {printf "%.1f%%", $3/$2 * 100.0}')"
 
 # CPU usage
 cpu_idle=$(top -bn1 | grep "Cpu(s)" | sed 's/.*, *\([0-9.]*\)%* id.*/\1/')
 cpu_usage=$(awk "BEGIN {printf \"%.1f%%\", 100 - $cpu_idle}")
-cpu_line="$ICON_CPU CPU $cpu_usage"
+cpu_line="$ICON_CPU $cpu_usage"
 
 # Volume
 # Volume using pactl (PipeWire-compatible)
@@ -56,8 +57,13 @@ brightness_max=$(brightnessctl max)
 brightness_percent=$(awk "BEGIN {printf \"%d%%\", ($brightness_raw / $brightness_max) * 100}")
 brightness_line="$ICON_BRIGHTNESS $brightness_percent"
 
-music="$ICON_MUSIC  $(playerctl metadata artist) - $(playerctl metadata title)"
+music_status=$(playerctl status 2>/dev/null)
+music=""
+
+if [ "$music_status" = "Playing" ]; then
+  music="$ICON_MUSIC  $(playerctl metadata artist) - $(playerctl metadata title)"
+fi
 
 # Final output
-echo "$wl_clipboard  $music  $date_line  $time_line  $battery_line  $vpn_line  $memory_usage  $cpu_line  $volume_line  $brightness_line  $updates_line"
+echo "$music   $date_line   $time_line   $battery_line   $vpn_line   $memory_usage   $cpu_line   $volume_line   $brightness_line   $updates_line"
 
