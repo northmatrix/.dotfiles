8d983ac2d1c51e93b3d55258ccf97e56d3bae4d1
Northmatrix <contact@northmatrix.co.uk>
2025-06-26 02:43:33 +0100

    a

--- i3status/.config/i3status/vpn_status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Example: checks if tun0 or any VPN interface is up
-if ip a | grep -q "nordlynx"; then
-    echo "󰖂 VPN ON"
-else
-    echo "󰖃 VPN OFF"
-fi
--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++
@@ -1,5 +0,0 @@
-require("nvchad.configs.lspconfig").defaults()
-
-local servers = { "html", "cssls", "pyright", "clangd", "gopls", "rust_analyzer" }
-
-vim.lsp.enable(servers)
